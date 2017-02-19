aba4de48622528ea186e0850e43276c904e2ff70
fromKK <info@fromkk.info>
2017-02-19 18:05:33 +0900

    Initial Commit

---
+++ Icon/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Icon
+//
+//  Created by Kazuya Ueoka on 2017/02/19.
+//  Copyright © 2017年 fromKK. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
